f280c25857d91c8566f647bbf1f5906fdec760dd
Victoriia-Ripka <vikaripka@gmail.com>
2022-12-08 20:28:41 +0100

    comments

--- contacts.js
+++ contacts.js
@@ -3,18 +3,22 @@ const fs = require('fs').promises;
 
 const contactsPath = path.resolve('./db/contacts.json');
 
-// TODO: задокументувати кожну функцію
+// зчитуємо всі дані з файлу
 const listContacts = async() =>  {
   const result = await fs.readFile(contactsPath)
   return result.toString()
 }
 
+// зчитуємо всі дані з файлу і методом перебору масива шукаємо потріьний контакт
 const getContactById = async (contactId) => {
   const contacts = JSON.parse(await fs.readFile(contactsPath))
   const contact = contacts.find(contact => contact.id === contactId)
   return contact
 }
 
+// зчитуємо всі дані з файлу і методом перебору масива шукаємо потрібний контак
+// якщо його не знайдено - повертаємо нал
+// якщо знайдено - вирізаємо його з маисва і повертаємо
 const removeContact = async (contactId) => {
   const contacts = JSON.parse(await fs.readFile(contactsPath))
   const index = contacts.findIndex(item => item.id === contactId)
@@ -24,6 +28,9 @@ const removeContact = async (contactId) => {
   return result
 }
 
+// зчитуємо всі дані з файлу
+// створюємо новий айдішник і об'єкт нового контакта
+// додаємо його в масив і перезаписуємо файл
 const addContact = async ({ name, email, phone }) => {
   const contacts = JSON.parse(await fs.readFile(contactsPath))
   newId = parseInt(contacts[contacts.length-1].id) + 1
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+// підключення власного модуля контакти і модуля командер
 const contacts = require('./contacts')
 const { Command } = require("commander");
 const program = new Command();
 
+// додаємо всі можливі варіанти значення параметрів з термінала
 program
   .option("-a, --action <type>", "choose action")
   .option("-i, --id <type>", "user id")
@@ -9,37 +11,38 @@ program
   .option("-e, --email <type>", "user email")
   .option("-p, --phone <type>", "user phone");
 
+// ініціалізація аргументів  
 program.parse(process.argv);
-
 const argv = program.opts();
 
-// TODO: рефакторить
+// функція приймає параметри з консолі та виконує потрібну дію
 const invokeAction = async ({ action, id, name, email, phone }) => {
-  switch (action) {
+    switch (action) {
+      //обробка дії "список" і вивід в термінал списку контактів
     case "list":
         const result = await contacts.listContacts()
         console.log(result)
         break;
-
+        //обробка дії "обрати" і вивід в термінал результату
     case "get":
         const contact = await contacts.getContactById(id)
         console.log(contact)
         break;
-
+        //обробка дії додавання і вивід в термінал доданого контакту
     case "add":
           const newContact = await contacts.addContact({ name, email, phone })
         console.log(newContact)
           break;
-
+        // обробка дії видалення і вивід в термінал видаленого коньакту
     case "remove":
           const contactDeleted = await contacts.removeContact(id)
           console.log(contactDeleted)
-      // ... id
       break;
-
+        //обробка всіх лівих екшенів
     default:
       console.warn("\x1B[31m Unknown action type!");
   }
 }
 
+//виклик функції при запуску додатку
 invokeAction(argv);
